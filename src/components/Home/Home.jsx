c4e33ae6331cce871a270a36bff3c5d3ecb5e3a5
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-07 01:19:52 -0500

    Set up home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/styles.css";
+import laptop from "../../assets/images/laptop.png";
+const Home = () => {
+  return (
+    <section class="home">
+      <aside>
+        <h1>Scott</h1>
+        <h1>Martel</h1>
+      </aside>
+      <img src={laptop} />
+    </section>
+  );
+};
+
+export default Home;
