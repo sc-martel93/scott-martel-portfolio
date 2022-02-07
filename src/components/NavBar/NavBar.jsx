53e54950dfce466ca81522726bbbe2c68baa9359
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-07 02:16:17 -0500

    Add NavBar.jsx

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/styles.css";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
