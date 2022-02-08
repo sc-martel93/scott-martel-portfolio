1adc79b9a6c7327c822bc24f8a2ce31763e2077a
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-08 03:30:03 -0500

    Code refactor

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,20 +4,7 @@ import "./styles/styles.css";
 import image from "../../assets/images/image.png";
 import leftBracket from "../../assets/images/left_bracket.png";
 import rightBracket from "../../assets/images/right_bracket.png";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDatabase, faServer } from "@fortawesome/free-solid-svg-icons";
-import {
-  faHtml5,
-  faCss3,
-  faJsSquare,
-  faReact,
-  faNodeJs,
-  faGitAlt,
-} from "@fortawesome/free-brands-svg-icons";
-
-// <span className="blue">&#123;</span>
-// <span className="blue">&#125;</span>
+import Skills from "./Skills/Skills";
 
 const About = () => {
   const [viewSkills, setViewSkills] = useState(false);
@@ -38,47 +25,7 @@ const About = () => {
         <a onClick={() => setViewSkills(!viewSkills)}>Skills</a>
         <a href="#">Resume</a>
       </section>
-      {viewSkills ? (
-        <section className="skills">
-          <ul>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faHtml5} /> HTML / CSS
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faCss3} />
-              SASS, Tailwind, Bootstrap, MaterialUI
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faJsSquare} />
-              Javascript
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faReact} />
-              React / Redux
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faNodeJs} />
-              NodeJS / ExpressJS
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faDatabase} />
-              SQL / MySQL / MongoDB
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faServer} />
-              Heroku / Firebase
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faGitAlt} />
-              Git / GitHub
-            </li>
-          </ul>
-        </section>
-      ) : (
-        <img className="image" src={image} />
-      )}
+      {viewSkills ? <Skills /> : <img className="image" src={image} />}
     </article>
   );
 };
---
+++ src/components/About/Skills/Skills.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./styles/styles.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDatabase, faServer } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faReact,
+  faNodeJs,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <ul>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faHtml5} /> HTML / CSS
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faCss3} />
+          SASS, Tailwind, Bootstrap, MaterialUI
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faJsSquare} />
+          Javascript
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faReact} />
+          React / Redux
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faNodeJs} />
+          NodeJS / ExpressJS
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faDatabase} />
+          SQL / MySQL / MongoDB
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faServer} />
+          Heroku / Firebase
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faGitAlt} />
+          Git / GitHub
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
