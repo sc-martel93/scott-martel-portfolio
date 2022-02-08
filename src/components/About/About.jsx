4a44e6b6bb057470cfe26c15284bf07a092d56d7
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-07 20:07:36 -0500

    Set up About page, modify sizing

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,17 @@ import "./styles/styles.css";
 const About = () => {
   return (
     <section id="about">
-      <h1>Hello World</h1>
+      <h3>&#123; Hello World &#125;</h3>
+      <p>
+        my name is Scott and I am a Boston based web developer. ever since I was
+        young I have been fascinated by computers and have had a passion for
+        creating things from scratch. Both have lead me into the wonderful world
+        of web development.
+      </p>
+      <div className="buttons">
+        <button>Skills</button>
+        <button>Resume</button>
+      </div>
     </section>
   );
 };
