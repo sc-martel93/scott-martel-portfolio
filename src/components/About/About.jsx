d09ed035c8cffac0beb538fce70e0505683c9152
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-08 02:25:51 -0500

    Add fontawesome and icons to skill list

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./styles/styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDatabase, faServer } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faReact,
+  faNodeJs,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
+
 const About = () => {
   return (
     <article id="about">
@@ -20,16 +31,39 @@ const About = () => {
       </div>
       <section className="skills">
         <ul>
-          <li>HTML / CSS</li>
-          <li>SASS, Tailwind, Bootstrap, MaterialUI</li>
-          <li>Javascript</li>
-          <li>React / Redux</li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faHtml5} /> HTML / CSS
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faCss3} />
+            SASS, Tailwind, Bootstrap, MaterialUI
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faJsSquare} />
+            Javascript
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faReact} />
+            React / Redux
+          </li>
         </ul>
         <ul>
-          <li>NodeJS / ExpressJS</li>
-          <li>SQL / MySQL / MongoDB</li>
-          <li>Heroku / Firebase</li>
-          <li>Git / GitHub</li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faNodeJs} />
+            NodeJS / ExpressJS
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faDatabase} />
+            SQL / MySQL / MongoDB
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faServer} />
+            Heroku / Firebase
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" icon={faGitAlt} />
+            Git / GitHub
+          </li>
         </ul>
       </section>
     </article>
