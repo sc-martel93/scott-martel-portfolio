10f5ee840c619728787c6350d58819eefe2288ea
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-11 01:57:06 -0500

    Create Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "./styles/styles.css";
 
 const Footer = () => {
   return (
     <footer>
-      <h4>Scott Martel</h4>
+      <section className="info">
+        <ul>
+          <li>sc.martel93@gmail.com</li>
+          <li>GitHub</li>
+          <li>LinkedIn</li>
+          <li>Boston</li>
+        </ul>
+      </section>
+      <section className="title">
+        <h3>Scott</h3>
+        <h3>Martel</h3>
+        <h4>Web Developer</h4>
+      </section>
     </footer>
   );
 };
