52dc5d88cf9368542d314d315089fae509151b68
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-12 08:48:51 -0500

    Refactor and restyle footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,12 @@ import "./styles/styles.css";
 const Footer = () => {
   return (
     <footer>
-      <section className="footer-info">
-        <ul>
-          <li>Email</li>
-          <li>GitHub</li>
-          <li>LinkedIn</li>
-          <li>Boston</li>
-        </ul>
-      </section>
-      <hr />
-      <section className="footer-title">
-        <h3 className="yellow">Scott</h3>
-        <h3>Martel</h3>
-        <h4>Web Developer</h4>
-      </section>
+      <ul>
+        <li>Email</li>
+        <li>GitHub</li>
+        <li>LinkedIn</li>
+        <li>Boston</li>
+      </ul>
     </footer>
   );
 };
