53d8fd5db559a4982342b6e08ae28402df494771
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-02-12 21:50:46 -0500

    Build project card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./styles/styles.css";
 
+import ChatBoxIMG from "../../assets/images/ChatBoxIMG.PNG";
+
 const Projects = () => {
   return (
     <section id="projects" className="projects">
       <h3>Projects</h3>
+      <section className="cards">
+        <article className="card">
+          <img src={ChatBoxIMG} />
+          <h4>Chat Box</h4>
+          <p>
+            A real time chat application created using Google firebase and
+            firestore database with google login authentication
+          </p>
+          <section className="project-buttons">
+            <a href="">Live</a>
+            <a href="">Code</a>
+          </section>
+        </article>
+      </section>
     </section>
   );
 };
