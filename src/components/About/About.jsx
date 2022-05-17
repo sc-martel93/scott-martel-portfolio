6419a04d1beabf6196d02ff94988deced3ac1306
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-17 17:53:02 -0400

    Changed about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,9 +16,9 @@ const About = () => {
         <img className="bracket" src={rightBracket} />
       </section>
       <p>
-        my name is Scott and I am a Boston based web developer. ever since I was
+        My name is Scott and I am a Boston based web developer. Ever since I was
         young I have been fascinated by computers and have had a passion for
-        creating things from scratch. Both have lead me into the wonderful world
+        creating things from scratch. This has lead me into the wonderful world 
         of web development.
       </p>
 
