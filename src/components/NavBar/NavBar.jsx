11e79be058ecd070ac73ec683c725a6b2e9fe310
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-18 15:47:29 -0400

    Add logo to nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./styles/styles.css";
 
+import logo from "../../assets/images/logo.png";
+
 const NavBar = () => {
   return (
     <nav id="nav">
+      <img src={logo} alt="logo" />
       <ul>
         <li>
           <a href="#about">About</a>
