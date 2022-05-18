46938e39379ccfb7ababde916cf03edffd528880
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-18 15:25:25 -0400

    Added second project card, fixed margins

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -65,8 +65,61 @@ const Projects = () => {
               </a>
           </section>
           
+          
           </section>
         </article>
+
+        <article className="card">
+          <img src={ChatBoxIMG} />
+          <section className="description">
+            <h4>Chat Box</h4>
+            <p>
+              An online chat room application that includes user authentication through
+              google and allows the users to communicate through text.
+            </p>
+            <section className="tech">
+              <ul>
+                <li>
+                 <FontAwesomeIcon className="icon" icon={faReact} />
+                  React JS
+                </li>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faCss3} />
+                  CSS
+                </li>
+               </ul>
+               <ul>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faGoogle} />
+                  Google Firebase
+                </li>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faGitAlt} />
+                  Git
+                </li>
+              </ul>
+            </section>
+            <section className="project-buttons">
+              <a
+                href="https://chat-app-cd6c7.web.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live
+              </a>
+              <a
+                href="https://github.com/sc-martel93/chat-app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Code
+              </a>
+          </section>
+          
+          
+          </section>
+        </article>
+
       </section>
     </section>
   );
