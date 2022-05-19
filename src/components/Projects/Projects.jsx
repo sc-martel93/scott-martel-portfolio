21bec399d702ab8de01f4b34fea982ec0071cb72
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-19 17:28:53 -0400

    Added portfolio to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,7 +12,8 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 import ChatBoxIMG from "../../assets/images/ChatBoxIMG.PNG";
-import CryptoTracker from '../../assets/images/CryptoTracker.png'
+import CryptoTracker from '../../assets/images/CryptoTracker.png';
+import PortfolioImg from '../../assets/images/Portfolio.png';
 
 const Projects = () => {
   return (
@@ -117,8 +118,56 @@ const Projects = () => {
                 Code
               </a>
           </section>
-          
-          
+          </section>
+        </article>
+
+        <article className="card">
+          <img src={PortfolioImg} alt="Crypto tracker" />
+          <section className="description">
+            <h4>My Portfolio Page</h4>
+            <p>
+              I designed my portfolio web page using Figma and implemented the page
+              using React JS and CSS. For the contact form I used EmailJS to handle
+              sending the message to me via email.
+            </p>
+            <section className="tech">
+              <ul>
+                <li>
+                 <FontAwesomeIcon className="icon" icon={faReact} />
+                  React JS
+                </li>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faCss3} />
+                  CSS
+                </li>
+               </ul>
+               <ul>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faGoogle} />
+                  Google Firebase
+                </li>
+                <li>
+                  <FontAwesomeIcon className="icon" icon={faGitAlt} />
+                  Git
+                </li>
+              </ul>
+            </section>
+            <section className="project-buttons">
+              <a
+                href="https://sc-martel93.github.io/Crypto-Tracker/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live
+              </a>
+              <a
+                href="https://github.com/sc-martel93/Crypto-Tracker/tree/master"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Code
+              </a>
+          </section>
           </section>
         </article>
 
