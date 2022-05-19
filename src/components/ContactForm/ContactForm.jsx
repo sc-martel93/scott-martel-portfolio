ab0a5823b9060e313d80333b7cae2471fa7c8bed
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-19 15:57:05 -0400

    Add button and max length for message input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import "./styles/styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+
 const ContactForm = () => {
   return (
       <section id="contact" className="contact">
       <h3>Contact Me</h3>
        <form className="contact_form">
-            <label>Name</label>
-            <input type="text" name="name" placeholder="Name..." />
-            <label>Email</label>
-            <input type="email" name="email" placeholder="example@email.com"  />
-            <label>Message</label>
-            <textarea name="message" placeholder="Message..." />
+            <label>Your Name:</label>
+            <input required type="text" name="name" placeholder="Name..." />
+            <label>Your Email: </label>
+            <input required type="email" name="email" placeholder="example@email.com"  />
+            <label>Your Message:</label>
+            <textarea required name="message" placeholder="Message..." maxlength="1500" />
+            <button type="submit">
+                Send
+                <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
         </form>
       </section>
    
