d758dec91ac77ef36cce3931153d010308d9dbad
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2022-05-24 11:48:40 -0400

    Fix html for nav bar ul

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,11 @@ const NavBar = () => {
       <ul>
         <li>
           <a href="#about">About</a>
+        </li>
+        <li>
           <a href="#projects">Projects</a>
+        </li>
+        <li>
           <a href="#contact">Contact</a>
         </li>
       </ul>
